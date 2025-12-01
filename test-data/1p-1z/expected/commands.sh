6665af09e7978f4e3265e316f0da4186d854da0a
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-12-01 22:00:32 +0100

    Remove accidentally included work from another PR.

--- test-data/1p-1z/expected/commands.sh
+++ test-data/1p-1z/expected/commands.sh
@@ -1,4 +1,3 @@
 cp out/policies/minimal.toml /etc/cascade/policies/
 cascade --server 127.0.0.1:4539 policy reload
-cascade --server 127.0.0.1:4539 hsm add --insecure --username OpenDNSSEC --password 1234 kmip2pkcs11
 cascade --server 127.0.0.1:4539 zone add --policy minimal --source minimal.zone somezone
