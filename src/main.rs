7c6bdca83f079331058d4a25e9eb653138b441e1
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2026-01-06 14:56:07 +0100

    cargo fmt.

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,10 @@ use cascade::policy::{
     ServerPolicy, SignerDenialPolicy, SignerPolicy, SignerSerialPolicy,
 };
 use domain::base::Ttl;
-use kmip2pkcs11_cfg::{daemonbase::process::{GroupId, UserId}, v1::LogLevel};
+use kmip2pkcs11_cfg::{
+    daemonbase::process::{GroupId, UserId},
+    v1::LogLevel,
+};
 use quick_xml::DeError;
 use schema::xml::addns::{Adapter, Outbound};
 use schema::xml::conf::Configuration;
