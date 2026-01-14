849f3c11071136a5ffe174621aafdad8e60c9d54
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2026-01-14 11:50:01 +0100

    Update test-data to correct failing tests.

--- test-data/1p-1z/expected/commands.sh
+++ test-data/1p-1z/expected/commands.sh
@@ -1,4 +1,15 @@
+# Copy the generated policies to the Cascade policy directory.
 sudo cp out/policies/minimal.toml /etc/cascade/policies/
+
+# Set the copied policy file ownership and permissions so that Cascade can read the files.
+sudo chown test /etc/cascade/policies/minimal.toml
+sudo chmod u+rx /etc/cascade/policies/minimal.toml
+
+# Tell Cascade to reload its policy files.
 cascade --server 127.0.0.1:4539 policy reload
+
+# Tell Cascade that a kmip2pkcs11 instance named 'somehsm' is available at 127.0.0.1.
 cascade --server 127.0.0.1:4539 hsm add --insecure --username OpenDNSSEC --password 1234 somehsm 127.0.0.1
+
+# Tell Cascade to load and sign our zones using the appropriate policies.
 cascade --server 127.0.0.1:4539 zone add --policy minimal --source minimal.zone somezone
--- test-data/1p-2z-2hsm/expected/commands.sh
+++ test-data/1p-2z-2hsm/expected/commands.sh
@@ -1,7 +1,22 @@
+# Copy the generated policies to the Cascade policy directory.
 sudo cp out/policies/hsm1.toml /etc/cascade/policies/
 sudo cp out/policies/hsm2.toml /etc/cascade/policies/
+
+# Set the copied policy file ownership and permissions so that Cascade can read the files.
+sudo chown test /etc/cascade/policies/hsm1.toml
+sudo chmod u+rx /etc/cascade/policies/hsm1.toml
+sudo chown test /etc/cascade/policies/hsm2.toml
+sudo chmod u+rx /etc/cascade/policies/hsm2.toml
+
+# Tell Cascade to reload its policy files.
 cascade --server 127.0.0.1:4539 policy reload
+
+# Tell Cascade that a kmip2pkcs11 instance named 'somehsm' is available at 127.0.0.1.
 cascade --server 127.0.0.1:4539 hsm add --insecure --username OpenDNSSEC --password 1234 somehsm 127.0.0.1
+
+# Tell Cascade that a kmip2pkcs11 instance named 'someotherhsm' is available at 127.0.0.1.
 cascade --server 127.0.0.1:4539 hsm add --insecure --username OpenDNSSEC --password 1234 someotherhsm 127.0.0.1
+
+# Tell Cascade to load and sign our zones using the appropriate policies.
 cascade --server 127.0.0.1:4539 zone add --policy hsm1 --source minimal.zone somezone
 cascade --server 127.0.0.1:4539 zone add --policy hsm2 --source minimal.zone someotherzone
